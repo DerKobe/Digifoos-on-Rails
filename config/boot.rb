a442b08cc35f035a8bfae6e878f65494dbc89cc3
Philip Claren <kobe@mixxt.net>
2014-06-20 10:44:14 +0200

    fixed db user

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
 ENV['RAILS_ENV'] ||= 'development'
-ENV['DATABASE_URL'] = "postgres://kobe@localhost/digifoos_#{ENV['RAILS_ENV']}"
+ENV['DATABASE_URL'] = "postgres://postgres@localhost/digifoos_#{ENV['RAILS_ENV']}"
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
