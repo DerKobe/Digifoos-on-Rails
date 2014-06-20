7debd4429ac63852ae962c8d32e37ee683745d83
Philip Claren <kobe@mixxt.net>
2014-06-20 10:57:34 +0200

    some stuff

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,6 +7,7 @@ class Game < ActiveRecord::Base
   belongs_to :player2, class_name: 'Player'
   belongs_to :player3, class_name: 'Player'
   belongs_to :player4, class_name: 'Player'
+  belongs_to :session
 
   # scopes .....................................................................
   # validations ................................................................
