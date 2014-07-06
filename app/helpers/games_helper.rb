821bfa6962262f4c097f7fa4c5f3a0da4f310a99
Philip Claren <kobe@mixxt.net>
2014-07-06 14:49:07 +0200

    view refactored

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,20 @@
 module GamesHelper
+
   def winner_loser_class(team)
     winner_class = 'winner'
     loser_class  = 'loser'
     team.points > 0 ? winner_class : loser_class
+  end
+
+  def date_indicator(game)
+    if @last_timestamp.try(:at_beginning_of_day) != game.created_at.at_beginning_of_day
+      @last_timestamp = game.created_at.at_beginning_of_day
+
+      content_tag(:tr, class: 'date-indicator') do
+        content_tag(:td, colspan: 3) do
+          l @last_timestamp.to_date, format: :long
+        end
+      end
+    end
   end
 end
