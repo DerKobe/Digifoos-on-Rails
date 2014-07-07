cd4ea100c858caca6077052ce9f206846b7260b6
Philip Claren <kobe@mixxt.net>
2014-07-07 09:22:41 +0200

    design changes

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -7,7 +7,7 @@ module GroupsHelper
         PlayersService.get_players_for(group, 3).collect.with_index do |player, index|
           concat(
               content_tag(:span, class: "position-#{index} trophy") do
-                fa_icon 'trophy', text: player.name
+                fa_stacked_icon 'trophy inverse', base: 'circle', class: "trophy position-#{index}", text: player.name
               end
           )
         end
