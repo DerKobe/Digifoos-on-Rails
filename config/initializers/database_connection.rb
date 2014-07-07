996312d747b619648d4652812a61016be87ae0e7
Philip Claren <kobe@mixxt.net>
2014-07-07 20:13:05 +0200

    heroku config 2

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -5,6 +5,8 @@ Rails.application.config.after_initialize do
     config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
     config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
-    ActiveRecord::Base.establish_connection(config)
+    config['url']               = ENV['HEROKU_POSTGRESQL_MAUVE_URL']
+
+    ActiveRecord::Base.establish_connection(config.slice('adapter', 'pool', 'reaping_frequency', 'url'))
   end
 end
