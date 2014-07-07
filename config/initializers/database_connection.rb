b4ee1953363daf31bf17d1d309df83f33efcfec9
Philip Claren <kobe@mixxt.net>
2014-07-07 20:23:27 +0200

    FINALLY!!!

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -2,11 +2,18 @@ Rails.application.config.after_initialize do
   ActiveRecord::Base.connection_pool.disconnect!
 
   ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+    config = if ENV['HEROKU_POSTGRESQL_MAUVE_URL'].present?
+               {
+                   'adapter' => 'postgresql',
+                   'url'     => ENV['HEROKU_POSTGRESQL_MAUVE_URL']
+               }
+             else
+               ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+             end
+
     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
     config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
-    config['url']               = ENV['HEROKU_POSTGRESQL_MAUVE_URL']
 
-    ActiveRecord::Base.establish_connection(config.slice('adapter', 'pool', 'reaping_frequency', 'url'))
+    ActiveRecord::Base.establish_connection(config)
   end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -10,9 +10,16 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # worker specific setup
   ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+    config = if ENV['HEROKU_POSTGRESQL_MAUVE_URL'].present?
+               {
+                   'adapter' => 'postgresql',
+                   'url'     => ENV['HEROKU_POSTGRESQL_MAUVE_URL']
+               }
+             else
+               ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+             end
+
     config['pool'] = ENV['MAX_THREADS'] || 5
-    config['url']  = ENV['HEROKU_POSTGRESQL_MAUVE_URL']
 
     ActiveRecord::Base.establish_connection(config.slice('adapter', 'pool', 'url'))
   end
