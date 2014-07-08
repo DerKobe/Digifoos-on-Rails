ccc40bea57f6651204ec31517a9cf8a5b6074aef
Philip Claren <kobe@mixxt.net>
2014-07-08 19:34:32 +0200

    status quo

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -1,19 +1,19 @@
-# Rails.application.config.after_initialize do
-#   ActiveRecord::Base.connection_pool.disconnect!
-#
-#   ActiveSupport.on_load(:active_record) do
-#     config = if ENV['HEROKU_POSTGRESQL_MAUVE_URL'].present?
-#                {
-#                    'adapter' => 'postgresql',
-#                    'url'     => ENV['HEROKU_POSTGRESQL_MAUVE_URL']
-#                }
-#              else
-#                ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
-#              end
-#
-#     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-#     config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
-#
-#     ActiveRecord::Base.establish_connection(config)
-#   end
-# end
+Rails.application.config.after_initialize do
+  ActiveRecord::Base.connection_pool.disconnect!
+
+  ActiveSupport.on_load(:active_record) do
+    config = if ENV['HEROKU_POSTGRESQL_MAUVE_URL'].present?
+               {
+                   'adapter' => 'postgresql',
+                   'url'     => ENV['HEROKU_POSTGRESQL_MAUVE_URL']
+               }
+             else
+               ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+             end
+
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
+
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
