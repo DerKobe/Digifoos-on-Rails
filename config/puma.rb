5e894b7c27ab38625820a38d3f5867abcb0e1e3e
Philip Claren <kobe@mixxt.net>
2014-07-08 19:27:30 +0200

    database config experiment 1

--- config/puma.rb
+++ config/puma.rb
@@ -7,20 +7,20 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = if ENV['HEROKU_POSTGRESQL_MAUVE_URL'].present?
-               {
-                   'adapter' => 'postgresql',
-                   'url'     => ENV['HEROKU_POSTGRESQL_MAUVE_URL']
-               }
-             else
-               ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
-             end
-
-    config['pool'] = ENV['MAX_THREADS'] || 5
-
-    ActiveRecord::Base.establish_connection(config.slice('adapter', 'pool', 'url'))
-  end
-end
+# on_worker_boot do
+#   # worker specific setup
+#   ActiveSupport.on_load(:active_record) do
+#     config = if ENV['HEROKU_POSTGRESQL_MAUVE_URL'].present?
+#                {
+#                    'adapter' => 'postgresql',
+#                    'url'     => ENV['HEROKU_POSTGRESQL_MAUVE_URL']
+#                }
+#              else
+#                ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+#              end
+#
+#     config['pool'] = ENV['MAX_THREADS'] || 5
+#
+#     ActiveRecord::Base.establish_connection(config.slice('adapter', 'pool', 'url'))
+#   end
+# end
