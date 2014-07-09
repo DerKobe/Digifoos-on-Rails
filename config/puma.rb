6c9c385b0f31cc993d72231d6806e94e7cfb3445
Philip Claren <kobe@mixxt.net>
2014-07-09 09:54:13 +0200

    thats the config i was looking for

--- config/puma.rb
+++ config/puma.rb
@@ -7,29 +7,10 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-# on_worker_boot do
-#   # worker specific setup
-#   ActiveSupport.on_load(:active_record) do
-#     config = if ENV['DATABASE_URL'].present?
-#                {
-#                    'adapter' => 'postgresql',
-#                    'url'     => ENV['DATABASE_URL']
-#                }
-#              else
-#                ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
-#              end
-#
-#     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-#     config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
-#
-#     ActiveRecord::Base.establish_connection(config)
-#   end
-# end
 on_worker_boot do
   # worker specific setup
   ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-        Rails.application.config.database_configuration[Rails.env]
+    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
     config['pool'] = ENV['MAX_THREADS'] || 16
     ActiveRecord::Base.establish_connection(config)
   end
