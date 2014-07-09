70d50eae81fd093d9508c1c6787800818953bd8b
Philip Claren <kobe@mixxt.net>
2014-07-09 09:50:23 +0200

    one more try

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
-ENV['RAILS_ENV'] ||= 'development'
-ENV['DATABASE_URL'] = "postgres://postgres@localhost/digifoos_#{ENV['RAILS_ENV']}"
-
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
--- config/puma.rb
+++ config/puma.rb
@@ -7,21 +7,30 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+# on_worker_boot do
+#   # worker specific setup
+#   ActiveSupport.on_load(:active_record) do
+#     config = if ENV['DATABASE_URL'].present?
+#                {
+#                    'adapter' => 'postgresql',
+#                    'url'     => ENV['DATABASE_URL']
+#                }
+#              else
+#                ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+#              end
+#
+#     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+#     config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
+#
+#     ActiveRecord::Base.establish_connection(config)
+#   end
+# end
 on_worker_boot do
   # worker specific setup
   ActiveSupport.on_load(:active_record) do
-    config = if ENV['DATABASE_URL'].present?
-               {
-                   'adapter' => 'postgresql',
-                   'url'     => ENV['DATABASE_URL']
-               }
-             else
-               ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
-             end
-
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-    config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
-
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+        Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
     ActiveRecord::Base.establish_connection(config)
   end
 end
